ae3bb655be02aabe6803b95706526700f9ef33cf
j-o-bazely <j.o.bazely@qmul.ac.uk>
2024-08-12 04:17:37 -0100

    Update Ch1.ReMap.4_BismarkAlign.sh

--- Scripts/Ch1.ReMap.4_BismarkAlign.sh
+++ Scripts/Ch1.ReMap.4_BismarkAlign.sh
@@ -72,73 +72,70 @@ echo "This sample has ${FILE_COUNT} files"
 
 if [ "$FILE_COUNT" -gt 2 ]; then
     echo "There are more than 2 files for this sample, so merging is needed"
-# Check if merged file exists yet -> only run alignment if it doesn't
+    # Check if merged file exists yet -> only run alignment if it doesn't
     if [[ -f ${OUT_DIR}/${SAMPLE}_deduplicated.bam ]]; then
-    echo -e "\n### Merged bam already exists for ${SAMPLE}. Not re-aligning ###\n"
+        echo -e "\n### Merged bam already exists for ${SAMPLE}. Not re-aligning ###\n"
     else
-    echo -e "\n### Merged bam does not exist for ${SAMPLE}. Continue to alignment ###\n"
-    
-    # Save unique flowcell info from fastq filenames to variable
-    FLOWCELLS=$(ls $READS_DIR | cut -d '_' -f 1,2,3 | uniq )
-    echo "The flowcell IDs for this sample are: ${FLOWCELLS}"
+        echo -e "\n### Merged bam does not exist for ${SAMPLE}. Continue to alignment ###\n"
+        
+        # Save unique flowcell info from fastq filenames to variable
+        FLOWCELLS=$(ls $READS_DIR | cut -d '_' -f 1,2,3 | uniq )
+        echo "The flowcell IDs for this sample are: ${FLOWCELLS}"
 
-    for FLOWCELL in $FLOWCELLS
+        for FLOWCELL in $FLOWCELLS
+        do
+            # Read group ID in the format FlowCell.Lane (replace underscore in filename)
+            rgid=$(echo $FLOWCELL | cut -d '_' -f 1,2 | sed 's/_/\./g')
 
-    do
-    # Read group ID in the format FlowCell.Lane (replace underscore in filename)
-    rgid=$(echo $FLOWCELL | cut -d '_' -f 1,2 | sed 's/_/\./g')
+            echo -e "\n### Starting alignment for flowcell: ${FLOWCELL}, sample: ${SAMPLE} ###\n"
 
-    echo -e "\n### Starting alignment for flowcell: ${FLOWCELL}, sample: ${SAMPLE} ###\n"
+            bismark \
+            -p ${REPCORES} \
+            --rg_sample $SAMPLE --rg_id $rgid \
+            -o $OUT_DIR \
+            --basename $FLOWCELL \
+            $GENOME_DIR \
+            -1 ${FLOWCELL}*_1_trim.fq.gz \
+            -2 ${FLOWCELL}*_2_trim.fq.gz 
 
-    bismark \
-    -p ${REPCORES} \
-    --rg_sample $SAMPLE --rg_id $rgid \
-    -o $OUT_DIR \
-    --basename $FLOWCELL \
-    $GENOME_DIR \
-    -1 ${FLOWCELL}*_1_trim.fq.gz \
-    -2 ${FLOWCELL}*_2_trim.fq.gz 
+            echo -e "\n### Finished alignment for flowcell: ${FLOWCELL}, sample: ${SAMPLE} ###\n"
+            
+            ### Run Bismark deduplication ### 
+            echo -e "\n### Starting deduplication for flowcell: ${FLOWCELL}, sample: ${SAMPLE} ###\n"
 
-    echo -e "\n### Finished alignment for flowcell: ${FLOWCELL}, sample: ${SAMPLE} ###\n"
-    
-        ### Run Bismark deduplication ### 
-    echo -e "\n### Starting deduplication for flowcell: ${FLOWCELL}, sample: ${SAMPLE} ###\n"
+            deduplicate_bismark --paired --bam \
+            --output_dir $OUT_DIR \
+            --outfile ${FLOWCELL}_pe \
+            ${OUT_DIR}/${FLOWCELL}_pe.bam
 
-    deduplicate_bismark --paired --bam \
-    --output_dir $OUT_DIR \
-    --outfile ${FLOWCELL}_pe \
-    ${OUT_DIR}/${FLOWCELL}_pe.bam
+            echo -e "\n### Finished deduplication for flowcell: ${FLOWCELL}, sample: ${SAMPLE} ###\n"
+        done
 
-    echo -e "\n### Finished deduplication for flowcell: ${FLOWCELL}, sample: ${SAMPLE} ###\n"
-    done
-
-    ### Merge deduplicated bams from all flowcells ###
-    
-    echo -e "\n### Starting merging deduplicated flowcell bams for ${SAMPLE} ###\n"
-    
-    samtools merge -@ ${NSLOTS} \
-    ${OUT_DIR}/${SAMPLE}_deduplicated.bam \
-    ${OUT_DIR}/*_pe.deduplicated.bam
-    
-    echo -e "\n### Finished merging deduplicated flowcell bams for ${SAMPLE} ###\n"
-    
+        ### Merge deduplicated bams from all flowcells ###
+        
+        echo -e "\n### Starting merging deduplicated flowcell bams for ${SAMPLE} ###\n"
+        
+        samtools merge -@ ${NSLOTS} \
+        ${OUT_DIR}/${SAMPLE}_deduplicated.bam \
+        ${OUT_DIR}/*_pe.deduplicated.bam
+        
+        echo -e "\n### Finished merging deduplicated flowcell bams for ${SAMPLE} ###\n"
     fi
 
     ### Delete all flowcell bams, only if merged bam exists ###
     
     if [[ -f ${OUT_DIR}/${SAMPLE}_deduplicated.bam ]]; then
-    echo -e "\n### Merged bam for has been created for ${SAMPLE} ###"
-    echo -e "\n### Deleting flowcell bams ###\n"
-    
-    # Remove flowcell bams whilst keeping all reports
-    rm ${OUT_DIR}/V*.bam
-    
-    echo -e "### Deleted flowcell bams for ${SAMPLE} ###\n"
-    
+        echo -e "\n### Merged bam for has been created for ${SAMPLE} ###"
+        echo -e "\n### Deleting flowcell bams ###\n"
+        
+        # Remove flowcell bams whilst keeping all reports
+        rm ${OUT_DIR}/V*.bam
+        
+        echo -e "### Deleted flowcell bams for ${SAMPLE} ###\n"
+        
     else
-    echo -e "\n### Merged bam does not exist for ${SAMPLE} ###"
-    echo -e "### Will not delete flowcell bams ###\n"
-    
+        echo -e "\n### Merged bam does not exist for ${SAMPLE} ###"
+        echo -e "### Will not delete flowcell bams ###\n"
     fi # End if statement for deleting flowcell bams
 
 
@@ -156,106 +153,100 @@ if [ "$FILE_COUNT" -gt 2 ]; then
 
 
     if [[ -f ${OUT_DIR}/${SAMPLE}_deduplicated.sorted_by_name.bam ]]; then
-    echo -e "\n### Sorted bam for has been created for ${SAMPLE} ###"
-    echo -e "\n### Deleting unsorted bam ###\n"
+        echo -e "\n### Sorted bam for has been created for ${SAMPLE} ###"
+        echo -e "\n### Deleting unsorted bam ###\n"
 
-    # Delete
-    rm ${OUT_DIR}/${SAMPLE}_deduplicated.bam
-    
-    echo -e "### Deleted unsorted bam for ${SAMPLE} ###\n"
-    
+        # Delete
+        rm ${OUT_DIR}/${SAMPLE}_deduplicated.bam
+        
+        echo -e "### Deleted unsorted bam for ${SAMPLE} ###\n"
+        
     else
-    echo -e "\n### Sorted bam does not exist for ${SAMPLE} ###"
-    echo -e "### Will not delete unsorted bams ###\n"
+        echo -e "\n### Sorted bam does not exist for ${SAMPLE} ###"
+        echo -e "### Will not delete unsorted bams ###\n"
     fi
-
 
     echo -e "### All done ###"
 
 
 else
+    ####        Pipeline 2: Files are merged but not correctly named         ####
+
+    ## Some samples have been pre-merged, but the flowcell IDs have been retained, rather than being replaced by sample ID
+    ## This pipeline first renames the files to match the sample ID
 
 
-####        Pipeline 2: Files are merged but not correctly named         ####
-
-## Some samples have been pre-merged, but the flowcell IDs have been retained, rather than being replaced by sample ID
-## This pipeline first renames the files to match the sample ID
+    ## WORK IN PROGRESS
 
 
-## WORK IN PROGRESS
+    ####        Pipeline 3: Files are merged AND correctly named            ####
+
+    ## Now all merged files should be 
+
+    ### Run Bismark alignment ###
+
+    echo -e "\n### Starting alignment for sample: ${SAMPLE} ###\n"
+
+    bismark \
+    -p ${REPCORES} \
+    --basename $SAMPLE \
+    -o $OUT_DIR \
+    $GENOME_DIR \
+    -1 ${SAMPLE}_1_trim.fq.gz \
+    -2 ${SAMPLE}_2_trim.fq.gz
+
+    echo -e "\n### Finished alignment for sample: ${SAMPLE} ###\n"
+
+    # NB. Automatically has _pe.bam added to end of basename by default
+
+      
+    ### Run Bismark deduplication ### 
+    echo -e "\n### Starting deduplication for sample: ${SAMPLE} ###\n"
+      
+    deduplicate_bismark --paired --bam \
+    --output_dir $OUT_DIR \
+    --outfile ${SAMPLE} \
+    ${OUT_DIR}/${SAMPLE}_pe.bam
+
+    # NB. basename has .deduplicated.bam added to end by default
+      
+    echo -e "\n### Finished deduplication for sample: ${SAMPLE} ###\n"
+      
 
 
-####        Pipeline 3: Files are merged AND correctly named            ####
+    ### Sort deduplicated bam by name ###
 
-## Now all merged files should be 
+    # -n flag sorts by read name (required for meth calling)
 
-##
+    echo -e "### Sorting deduplicated bam by name for ${SAMPLE} ###"
 
-if [[ -f ${OUT_DIR}/${SAMPLE}_deduplicated.bam ]]; then
-    echo -e "\n### Merged bam already exists for ${SAMPLE}. Not re-aligning ###\n"
-    else
-    echo -e "\n### Merged bam does not exist for ${SAMPLE}. Continue to alignment ###\n"
+    samtools sort -n -@ ${NSLOTS} \
+    -o ${OUT_DIR}/${SAMPLE}_deduplicated.sorted_by_name.bam \
+    ${OUT_DIR}/${SAMPLE}.deduplicated.bam
 
-### Run Bismark alignment ###
-
-echo -e "\n### Starting alignment for sample: ${SAMPLE} ###\n"
-
-bismark \
--p ${REPCORES} \
---basename $SAMPLE \
--o $OUT_DIR \
-$GENOME_DIR \
--1 ${SAMPLE}_1_trim.fq.gz \
--2 ${SAMPLE}_2_trim.fq.gz
-
-echo -e "\n### Finished alignment for sample: ${SAMPLE} ###\n"
-
-# NB. Automatically has _pe.bam added to end of basename by default
-
-  
-### Run Bismark deduplication ### 
-echo -e "\n### Starting deduplication for sample: ${SAMPLE} ###\n"
-  
-deduplicate_bismark --paired --bam \
---output_dir $OUT_DIR \
---outfile ${SAMPLE} \
-${OUT_DIR}/${SAMPLE}_pe.bam
-
-# NB. basename has .deduplicated.bam added to end by default
-  
-echo -e "\n### Finished deduplication for sample: ${SAMPLE} ###\n"
-  
+    echo -e "### Sorted deduplicated bam by name for ${SAMPLE} ###"
 
 
-### Sort deduplicated bam by name ###
+    ### Delete unsorted bam ###
 
-# -n flag sorts by read name (required for meth calling)
+    if [[ -f ${OUT_DIR}/${SAMPLE}_deduplicated.sorted_by_name.bam ]]; 
+    then
+        echo -e "\n### Sorted bam for has been created for ${SAMPLE} ###"
+        echo -e "\n### Deleting unsorted bam ###\n"
 
-echo -e "### Sorting deduplicated bam by name for ${SAMPLE} ###"
-
-samtools sort -n -@ ${NSLOTS} \
--o ${OUT_DIR}/${SAMPLE}_deduplicated.sorted_by_name.bam \
-${OUT_DIR}/${SAMPLE}.deduplicated.bam
-
-echo -e "### Sorted deduplicated bam by name for ${SAMPLE} ###"
-
-
-### Delete unsorted bam ###
-
-if [[ -f ${OUT_DIR}/${SAMPLE}_deduplicated.sorted_by_name.bam ]]; 
-  then
-      echo -e "\n### Sorted bam for has been created for ${SAMPLE} ###"
-      echo -e "\n### Deleting unsorted bam ###\n"
-
-      # Delete
-      rm ${OUT_DIR}/${SAMPLE}.deduplicated.bam
-      rm ${OUT_DIR}/${SAMPLE}_pe.bam
-     
-      echo -e "### Deleted unsorted bam for ${SAMPLE} ###\n"
+        # Delete
+        rm ${OUT_DIR}/${SAMPLE}.deduplicated.bam
+        rm ${OUT_DIR}/${SAMPLE}_pe.bam
        
-   else
-       echo -e "\n### Sorted bam does not exist for ${SAMPLE} ###"
-       echo -e "### Will not delete unsorted bams ###\n"
+        echo -e "### Deleted unsorted bam for ${SAMPLE} ###\n"
+         
+    else
+        echo -e "\n### Sorted bam does not exist for ${SAMPLE} ###"
+        echo -e "### Will not delete unsorted bams ###\n"
+    fi
+
+    echo -e "### All done ###"
 fi
 
-echo -e "### All done ###"
+
+
